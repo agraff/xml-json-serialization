a33d664b3018c5cb5eec623e74085586e31841f1
Alex Graff <alex.graff@7digital.com>
2014-09-09 16:10:48 +0100

    Tests now use the WebApiApplication (but its IIS Express must be started manually beforehand).

---
+++ WebApiApplication.Tests/SerialisationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Test.Common;
+
+namespace WebApiApplication.Tests
+{
+	[TestFixture]
+	public class SerialisationTests
+	{
+		[Test]
+		public void GetXml()
+		{
+			var client = CreateWebApiClient();
+
+			var content = client.GetXml("values");
+
+			Console.WriteLine(content);
+			Assert.That(content, Is.StringStarting("<"));
+		}
+
+		[Test]
+		public void GetJson()
+		{
+			var client = CreateWebApiClient();
+
+			var content = client.GetJson("values");
+
+			Console.WriteLine(content);
+			Assert.That(content, Is.StringStarting("{").Or.StringStarting("["));
+		}
+
+		private static ApiClient CreateWebApiClient()
+		{
+			return new ApiClient("http://localhost:57007/api");
+		}
+	}
+}

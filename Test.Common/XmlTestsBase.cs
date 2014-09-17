0f33a9c9664c77049b3ff58fbb953d7525ccd5ee
emma-ashley.liles <emma-ashley.liles@7digital.com>
2014-09-17 10:30:47 +0100

    Created an xml tests base object for xml tests to inherit from and help gradually move them all to the same tests for both endpoints

---
+++ Test.Common/XmlTestsBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace Test.Common
+{
+	public abstract class XmlTestsBase
+	{
+		protected abstract string ApiUrl { get; }
+
+		public string GetXmlResponse(string endpoint)
+		{
+			var Client = new ApiClient(ApiUrl);
+			var content = Client.GetXml(endpoint);
+			Console.WriteLine("--- Begin Content ---");
+			Console.WriteLine(content ?? "NULL");
+			Console.WriteLine("---- End Content ----");
+			return content;
+
+		}
+
+		[Test]
+		public void ContentIsNotEmpty()
+		{
+			var content = GetXmlResponse("tracks");
+			Assert.IsNotNullOrEmpty(content);
+		}
+
+
+		[Test]
+		public void ContentIsValidXml()
+		{
+			var content = GetXmlResponse("tracks");
+
+			var deserializedObject = XDocument.Parse(content);
+			Assert.That(deserializedObject, Is.Not.Null, "The deserialized object was null.");
+		}
+
+		[Test]
+		public void ContentMatchesExpectedXml()
+		{
+			var expectedContent = Test.Common.Properties.Resources.ExpectedXml;
+
+			var content = GetXmlResponse("tracks");
+
+			Assert.That(content, Is.EqualTo(expectedContent));
+		}
+
+
+	}
+}
--- WebApiApplication.Tests/XmlTests.cs
+++ WebApiApplication.Tests/XmlTests.cs
@@ -5,46 +5,11 @@ using Test.Common;
 
 namespace WebApiApplication.Tests
 {
-	public class XmlTests : SerialisationTestsBase
+	public class XmlTests : XmlTestsBase
 	{
-		[TestFixtureSetUp]
-		public void GetXmlResponse()
+		protected override string ApiUrl
 		{
-			var client = new ApiClient("http://localhost:57007/api");
-
-			Content = client.GetXml("tracks");
-		}
-
-		[Test]
-		public void ContentIsNotEmpty()
-		{
-			Assert.IsNotNullOrEmpty(Content);
-		}
-
-		[Test]
-		public void ContentIsValidXml()
-		{
-			var deserializedObject = XDocument.Parse(Content);
-			Assert.That(deserializedObject, Is.Not.Null, "The deserialized object was null.");
-		}
-
-		[Test]
-		public void ContentMatchesExpectedXml()
-		{
-			var expectedContent = Test.Common.Properties.Resources.ExpectedXml;
-
-			Assert.That(Content, Is.EqualTo(expectedContent));
-		}
-
-		[Test]
-		public void ContentMatchesExpectedXmlIgnoringWhitespace()
-		{
-			var expectedContent = Test.Common.Properties.Resources.ExpectedXml;
-
-			var contentWithoutWhitespace = Regex.Replace(Content, @"\s", "");
-			var expectedContentWithoutWhitespace = Regex.Replace(expectedContent, @"\s", "");
-
-			Assert.That(contentWithoutWhitespace, Is.EqualTo(expectedContentWithoutWhitespace));
+			get { return "http://localhost:57007/api"; }
 		}
 	}
 }

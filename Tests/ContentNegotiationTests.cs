cb2e29c9393a496aac25ed2ba6f282f6df2d9893
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-09-18 16:29:16 +0100

    Added content negotiation tests

---
+++ Tests/ContentNegotiationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RestSharp;
+
+namespace Tests
+{
+
+	[TestFixture("Nancy", "http://localhost/api-examples/nancy")]
+	[TestFixture("WebApi", "http://localhost/api-examples/webapi/api")]
+	public class ContentNegotiationTests
+	{
+		private readonly string _apiUrl;
+
+		public ContentNegotiationTests(string apiType, string apiUrl)
+		{
+			_apiUrl = apiUrl;
+		}
+
+		[Test]
+		public void Bad_content_type_returns_406_Not_Acceptable()
+		{
+			var client = new RestClient(_apiUrl);
+			var request = new RestRequest("track", Method.GET);
+			request.AddHeader("Accept", "application/foo");
+
+			var response = client.Execute(request);
+
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotAcceptable));
+			Assert.That(response.Content, Is.Empty);
+		}
+	}
+}

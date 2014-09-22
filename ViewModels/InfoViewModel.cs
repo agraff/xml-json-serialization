d5cb1450f3a6f849887d748c004c087ff9b428c5
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-09-22 17:14:50 +0100

    Cleanup

--- ViewModels/InfoViewModel.cs
+++ ViewModels/InfoViewModel.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 using System.Xml.Serialization;
 
 namespace ViewModels
 {
 	[XmlRoot("info")]
-	[DataContract(Namespace = "info")]
 	public class InfoViewModel
 	{
 		[XmlElement("technology")]
--- ViewModels/TrackViewModel.cs
+++ ViewModels/TrackViewModel.cs
@@ -5,23 +5,18 @@ using System.Xml.Serialization;
 namespace ViewModels
 {
 	[XmlRoot("track")]
-	[DataContract(Namespace = "track")]
 	public class TrackViewModel
 	{
 		[XmlElement("title")]
-		[DataMember]
 		public string Title { get; set; }
 
 		[XmlElement("number")]
-		[DataMember]
 		public int Number { get; set; }
 
 		[XmlElement("releaseDateTime")]
-		[DataMember]
 		public DateTime ReleaseDateTime { get; set; }
 
 		[XmlElement("type")]
-		[DataMember]
 		public TypeEnum Type { get; set; }
 	}
 }

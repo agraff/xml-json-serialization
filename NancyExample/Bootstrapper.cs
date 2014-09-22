d22aee5a3eda4feb2c877e13301456eb4b4565b4
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-09-22 18:02:27 +0100

    Moved nancy boostrapper to a the SD.Serialization.Unwrapped.Nancy project

--- NancyExample/Bootstrapper.cs
+++ NancyExample/Bootstrapper.cs
@@ -1,10 +1,7 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
-using Nancy.Responses.Negotiation;
-using Nancy.Serialization.JsonNet;
 using Nancy.TinyIoc;
-using NancyExample.Serializers;
-using Newtonsoft.Json;
+using SevenDigital.Serialization.Unwrapped.Nancy;
 
 namespace NancyExample
 {
@@ -12,31 +9,14 @@ namespace NancyExample
 	{
 		protected override NancyInternalConfiguration InternalConfiguration
 		{
-			get { return new NancyInternalConfigurationFactory().Build(); }
+			get { return NancyBootstrapper.GetInternalConfiguration(); }
 		}
 
 		protected override void ConfigureApplicationContainer(TinyIoCContainer container)
 		{
 			base.ConfigureApplicationContainer(container);
-			container.Register(typeof(JsonSerializer), typeof(ExampleJsonSerializer));
-		}
-	}
-
-	public class NancyInternalConfigurationFactory
-	{
-		public NancyInternalConfiguration Build()
-		{
-			return NancyInternalConfiguration.WithOverrides(c =>
-			{
-				// This is optional because of Nancy's serializers autodiscovery and priority
-				c.Serializers.Clear();
-				c.Serializers.Add(typeof(ExampleXmlSerializer));
-				c.Serializers.Add(typeof(JsonNetSerializer));
-
-				c.ResponseProcessors.Clear();
-				c.ResponseProcessors.Add(typeof(XmlProcessor));
-				c.ResponseProcessors.Add(typeof(ExampleJsonProcessor));
-			});
+			NancyBootstrapper.ConfigureApplicationContainer(container);
+			
 		}
 	}
 }
---
+++ SevenDigital.Serialization.Unwrapped.Nancy/NancyBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Nancy.Bootstrapper;
+using Nancy.Responses.Negotiation;
+using Nancy.Serialization.JsonNet;
+using Nancy.TinyIoc;
+using NancyExample.Serializers;
+using Newtonsoft.Json;
+
+namespace SevenDigital.Serialization.Unwrapped.Nancy
+{
+	public static class NancyBootstrapper
+	{
+		public static NancyInternalConfiguration GetInternalConfiguration()
+		{
+			return NancyInternalConfiguration.WithOverrides(c =>
+			{
+				// This is optional because of Nancy's serializers autodiscovery and priority
+				c.Serializers.Clear();
+				c.Serializers.Add(typeof(ExampleXmlSerializer));
+				c.Serializers.Add(typeof(JsonNetSerializer));
+
+				c.ResponseProcessors.Clear();
+				c.ResponseProcessors.Add(typeof(XmlProcessor));
+				c.ResponseProcessors.Add(typeof(ExampleJsonProcessor));
+			});
+		}
+
+		public static void ConfigureApplicationContainer(TinyIoCContainer container)
+		{
+			container.Register(typeof(JsonSerializer), typeof(ExampleJsonSerializer));
+		}
+	}
+}
